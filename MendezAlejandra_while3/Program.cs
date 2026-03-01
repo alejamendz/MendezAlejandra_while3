edca02819ce7b4bafa25778219da6db163c16f6c
Alejandra <Alejandra@DESKTOP-HQKA5R3>
2026-03-01 13:46:17 -0600

    uso de while y fin del programa

--- MendezAlejandra_while3/Program.cs
+++ MendezAlejandra_while3/Program.cs
@@ -1,2 +1,9 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Mostrar los números pares del 1 al 20");
+
+int i = 2;
+while (i <= 20)
+{
+    Console.WriteLine(i);
+    i += 2;
+}
